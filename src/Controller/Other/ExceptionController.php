4cdb4c27233e6a91e46fc25592898d7f117ba3e7
cgirard <camille.girard1995@gmail.com>
2025-01-30 14:27:14 +0100

    add error 404 and 403

---
+++ src/Controller/Other/ExceptionController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Other;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class ExceptionController
+{
+    private $twig;
+    private $kernel;
+
+    public function __construct(Environment $twig, KernelInterface $kernel)
+    {
+        $this->twig = $twig;
+        $this->kernel = $kernel;
+    }
+
+    /**
+     * @Route("/error", name="error")
+     */
+    public function show(HttpExceptionInterface $exception): Response
+    {
+        $statusCode = $exception->getStatusCode();
+        $template = $statusCode === 404 ? 'bundles/TwigBundle/Exception/error404.html.twig' : 'bundles/TwigBundle/Exception/error403.html.twig';
+
+        $content = $this->twig->render($template, [
+            'exception' => $exception,
+        ]);
+
+        return new Response($content, $statusCode);
+    }
+}
