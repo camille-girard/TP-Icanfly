aa03ba1080d0d2a6294523343b8a9d9cceb62769
Philippe Delente <philippe.delente@gmail.com>
2025-02-01 12:27:03 +0100

    add test functionnally

--- tests/Unit/MissionTest.php
+++ tests/Unit/MissionTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Tests;
+namespace App\Tests\Unit;
 
 use App\Entity\Mission;
 use Doctrine\ORM\EntityManagerInterface;
--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Tests;
+namespace App\Tests\Unit;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
