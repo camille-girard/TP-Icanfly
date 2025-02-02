72f3b15e952a9e72a720f186e77c2da8f32cfbdb
Philippe Delente <philippe.delente@gmail.com>
2025-02-02 13:44:43 +0100

    add flights + fix exception error

--- src/Controller/Other/FlightController.php
+++ src/Controller/Other/FlightController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller\Other;
 
+use App\Repository\MissionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +12,25 @@ use Symfony\Component\Routing\Attribute\Route;
 class FlightController extends AbstractController
 {
     #[Route(path: '/flight', name: 'page_flight')]
-    public function index(): Response
-    {
-        return $this->render('parts/flights/flight.html.twig');
+    public function index(
+        MissionRepository $missionRepository,
+    ): Response {
+        $flights = $missionRepository->findAllMissions(page: 3);
+
+        return $this->render('parts/flights/flight.html.twig', [
+            'flights' => $flights,
+        ]);
+    }
+
+    #[Route(path: '/flight/{id}', name: 'page_flight_show')]
+    public function show(
+        MissionRepository $missionRepository,
+        int $id,
+    ): Response {
+        $flight = $missionRepository->find($id);
+
+        return $this->render('parts/flights/flight-detail.html.twig', [
+            'flight' => $flight,
+        ]);
     }
 }
